c4fd3dbb744c748a854fa871192c71e2fe1aa3f1
Adhithi28K <157336234+Adhithi28K@users.noreply.github.com>
2024-12-06 00:46:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
 });
